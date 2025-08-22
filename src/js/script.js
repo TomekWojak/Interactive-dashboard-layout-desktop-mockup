d44572fc731561e36ca0b0e9f5a5e0415e08630f
Tomek <tomek.wojak796@gmail.com>
2025-08-22 18:37:25 +0200

    added some cards and animations for better look

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const chart = document.querySelector(".statistics__chart");
+	const reportBtn = document.querySelector(".report__btn");
+
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+
+	const btnClickAnimation = (e) => {
+		const circle = document.createElement("span");
+		circle.classList.add("circle");
+
+		const rect = e.target.getBoundingClientRect();
+
+		const top = e.clientY - rect.top + "px";
+		const left = e.clientX - rect.left + "px";
+
+		circle.style.top = top;
+		circle.style.left = left;
+
+		e.target.append(circle);
+
+		setTimeout(() => {
+			circle.remove();
+		}, 300);
+	};
+
+	const createColumns = () => {
+		for (let i = 0; i < months.length; i++) {
+			const box = document.createElement("div");
+			const column = document.createElement("div");
+			const monthName = document.createElement("span");
+			box.classList.add("statistics__box");
+			monthName.classList.add("statistics__month");
+			column.classList.add("statistics__column");
+
+			monthName.textContent = months[i];
+
+			box.append(column);
+			box.append(monthName);
+			chart.append(box);
+		}
+		const allColumns = Array.from(
+			document.getElementsByClassName("statistics__column")
+		);
+
+		if (allColumns) {
+			setTimeout(() => {
+				allColumns.forEach((column) => {
+					const colHeight = Math.floor(Math.random() * 80 + 20);
+					column.style.height = colHeight + "px";
+				});
+			}, 100);
+		}
+	};
+
+	createColumns();
+	reportBtn.addEventListener("click", btnClickAnimation);
+});
