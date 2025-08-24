d82d1582038eefc239fba4a8460e1cc8ff10bdc3
Tomek <tomek.wojak796@gmail.com>
2025-08-24 22:35:36 +0200

    added pro subscription button and some effects

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const { src, dest, series, parallel, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
-const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
@@ -22,7 +21,6 @@ function sassCompiler(done) {
 	src(paths.sass)
 		.pipe(sourcemaps.init())
 		.pipe(sass().on("error", sass.logError))
-		.pipe(autoprefixer())
 		.pipe(cssnano())
 		.pipe(rename({ suffix: ".min" }))
 		.pipe(sourcemaps.write())
